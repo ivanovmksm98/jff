0a2f22d9ad6607e44be1f175effc1a82d31f475d
ivanovmksm98 <33741406+ivanovmksm98@users.noreply.github.com>
2020-12-26 13:25:04 +0900

    Create jff.py

---
+++ jff.py
@@ -0,0 +1,17 @@
+from EducationYear import EducationYear
+from Subject import Subject
+from Crosssection import CrossSection
+from Student import Student
+
+
+class BRSPoints:
+    def __init__(self, subject: Subject, year: EducationYear, cross_section: CrossSection, points: int):
+        self.subject = subject
+        self.year = year
+        self.cross_section = cross_section
+        self.points = points
+
+    def __str__(self):
+        template = 'Subject: {} Year: {} Srez: {} Points: {}'
+        return template.format(self.subject.name, str(self.year.beginYear) + '-' + str(self.year.endYear),
+                               self.cross_section.name, self.points)
